da3b3b55a892e13f8f61cb252a6c2b3791ef6bdb
AllenDodul <allendodul6@gmail.com>
2022-07-15 12:28:40 +0600

    1 file modified

--- src/AddTodo.jsx
+++ src/AddTodo.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 function AddTodo(){
-  const handleForm = () => {
+  const [todo,setTodo] = useState('');
+
+  const handleInput= (e) =>{
+    setTodo(e.target.value);
+  }
+  const handleForm = (e) => {
     e.preventDefault();
+    console.log(todo);
   }
 
   return(
     <form onSubmit={handleForm}>
-      <input type="text" placeholder="Add Todo"/>
+      <input type="text" placeholder="Add Todo" onChange={handleInput} value={todo}/>
       <button>Add</button>
     </form>
   )
